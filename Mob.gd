0c9822bba5be177dc38a0a38ad9192862c360e52
Brian Vose <BRVose@gmail.com>
2023-10-30 19:40:19 -0600

    mob logic started

--- Mob.gd
+++ Mob.gd
@@ -6,5 +6,11 @@ extends CharacterBody3D
 func _physics_process(delta):
 	move_and_slide()
 	
-
+func initialize(start_pos, player_pos):
+	look_at_from_position(start_pos, player_pos, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
